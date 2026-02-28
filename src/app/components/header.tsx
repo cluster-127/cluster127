4373e5c8dcdc13d0c862c34dbad53aed76067306
laphilosophia <2537198+laphilosophia@users.noreply.github.com>
2026-02-28 23:54:57 +0300

    chore: logo update

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -19,14 +19,13 @@ export default function Header() {
         <Link href="/" className="flex items-center gap-4 z-20 p-4">
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
             viewBox="0 0 700 700"
             className="text-white size-6">
             <path
               fill="#fff"
-              d="M350 440c49.706 0 90-40.294 90-90s-40.294-90-90-90-90 40.294-90 90 40.294 90 90 90Z"
+              d="M350 440c49.706 0 90-40.294 90-90s-40.294-90-90-90-90 40.294-90 90 40.294 90 90 90"
             />
-            <path fill="#fff" d="M635 635H65V295h20v320h530V85H295V65h340v570Z" />
+            <path fill="#fff" d="M655 655H45V207.635l60 60V595h490V105H267.635l-60-60H655z" />
           </svg>
           <span className="text-light tracking-[0.4em] uppercase text-white">Cluster 127</span>
         </Link>
